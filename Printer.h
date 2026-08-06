f01c05535d69c7bd58f883c192dc9b013eacbea9
Mr_HedgeHog_UA <60219855+HedgeHog777@users.noreply.github.com>
2026-08-06 17:46:12 +0200

    Refactor Printer class with new constants and methods

--- Printer.h
+++ Printer.h
@@ -1,13 +1,53 @@
-enum class PrinterState
+#pragma once
+
+constexpr int MAX_PAPER = 100;
+constexpr int MAX_INK = 50;
+constexpr float ERROR_PROBABILITY = 0.05f;
+
+enum PrinterState
 {
-    ...
+    OFF,
+    READY,
+    NOINK,
+    NOPAPER,
+    ERROR
 };
+
+extern const char PrinterStateName[5][10];
 
 class Printer
 {
-public:
-    ...
-
 private:
-    ...
+    bool Power;
+    int State;
+    int Paper;
+    float Ink;
+
+    void SetState(int);
+
+public:
+    void Reset();
+
+    void TurnOn();
+    void TurnOff();
+
+    bool GetPower() { return Power; }
+
+    int LoadPaper(int);
+    int UnloadPaper(int);
+
+    int GetPaper() { return Paper; }
+
+    int LoadInk(int);
+
+    int GetInk() { return Ink; }
+
+    int Print(int, int);
+
+    int GetState();
+
+    const char* const GetStateName()
+    {
+        return PrinterStateName[State];
+    }
 };
